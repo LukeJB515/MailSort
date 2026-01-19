3f6bb7c99bd0e8850eb92c2c03276ed96d89d3d1
LukeJB515 <lukeest1984@gmail.com>
2026-01-19 17:01:10 -0600

    implemented console printer logic, next will resolve program bugs

--- LetterOpener/ConsolePrinter.cs
+++ LetterOpener/ConsolePrinter.cs
@@ -8,5 +8,19 @@ namespace LetterOpener
 {
     public class ConsolePrinter
     {
+        public void PrintSummary(IEnumerable<IGrouping<string, MailItem>> groupedMail)
+        {
+            foreach (var group in groupedMail)
+            {
+                Console.WriteLine($"Sender: {group.Key}");
+
+                foreach (var mail in group)
+                {
+                    Console.WriteLine($"  {mail.ReceivedDate:d} | {mail.Subject}");
+                }
+
+                Console.WriteLine();
+            }
+        }
     }
 }
