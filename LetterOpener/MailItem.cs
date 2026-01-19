f880ad29999567fd35af986a8e845254a0b8a8f5
LukeJB515 <lukeest1984@gmail.com>
2026-01-19 16:38:41 -0600

    init commit

---
+++ LetterOpener/MailItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterOpener
+{
+    public class MailItem
+    {
+        public MailPriority Priority { get; }
+        //public bool ShouldPrint => 
+    }
+
+    public enum MailRepository
+    {
+        Low,
+        Normal,
+        High
+    }
+}
---
+++ LetterOpener/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterOpener
+{
+    class Program
+    {
+        static void Main()
+        {
+            var repository = new MailRepository();
+            var printService = new MailPrintService();
+            var printer = new ConsolePrinter();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
